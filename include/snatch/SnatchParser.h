ddd2db3aad7d2930fa6c3291aca9a9225a87ab08
Tim P <timothy.e.potter@gmail.com>
2016-12-17 17:48:16 +0100

    Basic event parsing, untested.

--- include/snatch/SnatchParser.h
+++ include/snatch/SnatchParser.h
@@ -28,6 +28,28 @@ public:
 	void parse(uint8_t byte);
 private:
 	SnatchParserListener *listener_;
+
+	bool (SnatchParser::*current_event_parser_)(uint8_t);
+
+	snatch_imu_event_t imu_event_;
+	bool imuEventParser(uint8_t byte);
+
+	snatch_rx_event_t rx_event_;
+	bool rxEventParser(uint8_t byte);
+
+	snatch_status_event_t status_event_;
+	bool statusEventParser(uint8_t byte);
+
+	uint8_t field_index_;
+	uint8_t field_byte_index_;
+
+	uint16_t checksum_;
+	bool checksum_low_high_;
+	void addToCheckSum(uint8_t byte);
+
+	uint16_t checksum_read_;
+
+	bool parseInt(uint8_t bytesPerInt, uint8_t byte, uint8_t *value, uint8_t elementCount = 1);
 };
 
 }
--- src/SnatchParser.cpp
+++ src/SnatchParser.cpp
@@ -9,13 +9,163 @@
 
 namespace snatch {
 
-SnatchParser::SnatchParser(SnatchParserListener *listener) : listener_(listener) {
-	// TODO Auto-generated constructor stub
+SnatchParser::SnatchParser(SnatchParserListener *listener) :
+		listener_(listener), current_event_parser_(NULL), field_index_(0), field_byte_index_(0), checksum_(0), checksum_low_high_(false), checksum_read_(0) {
 
 }
 
 void SnatchParser::parse(uint8_t byte) {
+	if (current_event_parser_ != NULL) {
+		if ((this->*current_event_parser_)(byte)) {
+			// currentEventParser_ accepted byte. All good.
+			return;
+		} else {
+			// Reset on event parsed or error.
+			current_event_parser_ = NULL;
+		}
+	}
 
+	switch (byte) {
+	case 'I':
+		current_event_parser_ = &SnatchParser::imuEventParser;
+		break;
+	case 'R':
+		current_event_parser_ = &SnatchParser::rxEventParser;
+		break;
+	case 'S':
+		current_event_parser_ = &SnatchParser::statusEventParser;
+		break;
+	default:
+		// Ignore byte.
+		return;
+	}
+
+	field_index_ = 0;
+	field_byte_index_ = 0;
+	checksum_ = 0;
+	checksum_low_high_ = false;
+	addToCheckSum(byte); // The event type char is included in the checksum.
+}
+
+bool SnatchParser::imuEventParser(uint8_t byte) {
+	switch (field_index_) {
+	case 0:
+		parseInt(4, byte, (uint8_t*) &imu_event_.time);
+		break;
+	case 1:
+		parseInt(2, byte, (uint8_t*) &imu_event_.roll);
+		break;
+	case 2:
+		parseInt(2, byte, (uint8_t*) &imu_event_.pitch);
+		break;
+	case 3:
+		parseInt(2, byte, (uint8_t*) &imu_event_.yaw);
+		break;
+	case 4:
+		parseInt(2, byte, (uint8_t*) &imu_event_.accX);
+		break;
+	case 5:
+		parseInt(2, byte, (uint8_t*) &imu_event_.accY);
+		break;
+	case 6:
+		parseInt(2, byte, (uint8_t*) &imu_event_.accZ);
+		break;
+	case 7:
+		parseInt(2, byte, (uint8_t*) &imu_event_.gyroX);
+		break;
+	case 8:
+		parseInt(2, byte, (uint8_t*) &imu_event_.gyroY);
+		break;
+	case 9:
+		parseInt(2, byte, (uint8_t*) &imu_event_.gyroZ);
+		break;
+	case 10:
+		parseInt(2, byte, (uint8_t*) &imu_event_.magX);
+		break;
+	case 11:
+		parseInt(2, byte, (uint8_t*) &imu_event_.magY);
+		break;
+	case 12:
+		parseInt(2, byte, (uint8_t*) &imu_event_.magZ);
+		break;
+	case 13: // Checksum.
+		if (parseInt(2, byte, (uint8_t*) &checksum_read_)) {
+			if (checksum_ == checksum_read_) {
+				listener_->handleParserEvent(&imu_event_);
+			}
+			return false;
+		}
+		return true;
+	default:
+		return false;
+	}
+	addToCheckSum(byte);
+	return true;
+}
+
+bool SnatchParser::rxEventParser(uint8_t byte) {
+	switch (field_index_) {
+	case 0: // Time
+		parseInt(4, byte, (uint8_t*) &rx_event_.time);
+		break;
+	case 1: // Channels
+		parseInt(2, byte, (uint8_t*) &rx_event_.channels[field_byte_index_ >> 1], snatch::RxChannelCount);
+		break;
+	case 2: // Checksum.
+		if (parseInt(2, byte, (uint8_t*) &checksum_read_)) {
+			if (checksum_ == checksum_read_) {
+				listener_->handleParserEvent(&rx_event_);
+			}
+			return false;
+		}
+		return true;
+	default:
+		return false;
+	}
+	addToCheckSum(byte);
+	return true;
+}
+
+bool SnatchParser::statusEventParser(uint8_t byte) {
+	switch (field_index_) {
+	case 0: // Time
+		parseInt(4, byte, (uint8_t*) &status_event_.time);
+		break;
+	case 1: // Channels
+		parseInt(2, byte, (uint8_t*) &status_event_.channels[field_byte_index_ >> 1], snatch::RxChannelCount);
+		break;
+	case 2: // Checksum.
+		if (parseInt(2, byte, (uint8_t*) &checksum_read_)) {
+			if (checksum_ == checksum_read_) {
+				listener_->handleParserEvent(&status_event_);
+			}
+			return false;
+		}
+		return true;
+	default:
+		return false;
+	}
+	addToCheckSum(byte);
+	return true;
+}
+
+void SnatchParser::addToCheckSum(uint8_t byte) {
+	checksum_ += (byte << (checksum_low_high_ ? 0 : 8));
+	checksum_low_high_ = !checksum_low_high_;
+}
+
+// Reads one or more ints into give value address.
+bool SnatchParser::parseInt(uint8_t bytesPerInt, uint8_t byte, uint8_t *value, uint8_t elementCount) {
+	uint8_t i = field_byte_index_++ % bytesPerInt;
+	value[i++] = byte;
+
+	if (field_byte_index_ != bytesPerInt * elementCount) {
+		return false;
+	}
+
+	field_byte_index_ = 0;
+	field_index_++;
+	return true;
 }
 
 }
